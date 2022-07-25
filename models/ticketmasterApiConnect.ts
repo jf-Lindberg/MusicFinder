2768464168f437cc0588592267bef0e304757b50
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-07-25 17:42:46 +0200

    Added primitive search

--- models/ticketmasterApiConnect.ts
+++ models/ticketmasterApiConnect.ts
@@ -1,13 +1,18 @@
 import Ticketmaster from "../constants/Ticketmaster";
 
-export default async function findEvents() {
-    const url = Ticketmaster.url + '/events?domain=sweden&countryCode=SE&size=200&classificationName=[music]&apikey=' + Ticketmaster.key;
+export default async function findEvents(searchTerm: string = '') {
+    const url = Ticketmaster.url + '/events?keyword=' + searchTerm + '&domain=sweden&countryCode=SE&size=200&classificationName=[music]&apikey=' + Ticketmaster.key;
     console.log('Ticketmaster API: Fetching...');
     const response = await fetch(url)
         .then(r => r.json())
         .catch(err => {
             console.error('Request failed', err)
         })
+
+    if (response.page.totalElements === 0) {
+        console.log('Ticketmaster API: Search failed. No events match that search term.');
+        throw new Error('No events match that search term');
+    }
 
     console.log('Ticketmaster API: Done loading. Events array length: ' + response._embedded.events.length);
     return await response._embedded.events;
